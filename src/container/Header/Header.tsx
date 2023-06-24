af95f26dec4938cee708bfcf1009fdf88a68ddb1
Tetiana <kozaktanya077@gmail.com>
2023-06-24 18:27:58 +0300

    add: header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,5 +1,18 @@
+import { Container } from '@mui/material'
+import './Header.scss'
+
 type Props = {}
+
 const Header = (props: Props) => {
-  return <div>Header</div>
+  return (
+    <header>
+      <Container>
+        <div className="header-logo">
+          <h3 className="header-title">InCode</h3>
+          <h5 className="header-subtitle">Finance</h5>
+        </div>
+      </Container>
+    </header>
+  )
 }
 export default Header
